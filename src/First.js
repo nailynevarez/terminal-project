ff4c66a9c0b03f6e763250efbb90a5e1f6f68347
nailynevarez <nailynevarez@gmail.com>
2020-04-10 16:48:32 -0700

    Finish Eagle for First Scene

--- src/First.js
+++ src/First.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react';
 import FirstBackground from "./images/First-Background.jpg";
 import FirstTexture from "./images/First-Texture.gif";
+import FirstEagle from "./images/First-Eagle.gif";
 
 export default class First extends Component {
   constructor(props){
     super(props);
     this.state = {
       isPageActive: true,
+      isEagleActive: false,
     };
+  }
+
+  componentDidMount(){
+    this.showEagle();
+  }
+
+  showEagle = () => {
+    setTimeout(() => {
+      this.setState({
+        isEagleActive: true,
+      });
+    }, 2000);
   }
 
   // handleBeginClick = () => {
@@ -29,6 +43,7 @@ render() {
   return (
     <div className = {this.state.isPageActive ? 'fadeIn' : 'fadeOut'}>
     <div className = "First-Wrapper">
+    {this.state.isEagleActive ? <img className = "First-Eagle" src = {FirstEagle}/> : null}
     <img className = "First-Texture" src = {FirstTexture}/>
     <img className = "First-Background" src = {FirstBackground} />
     </div>
