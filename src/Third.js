7788edb422b320cc1ff7a6b0a668084cf2e834e7
nailynevarez <nailynevarez@gmail.com>
2020-04-29 13:21:21 -0700

    Insert distorted flag

--- src/Third.js
+++ src/Third.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import ThirdFlagLarge from "./images/Third-FlagLarge.gif";
 import ThirdFlagSmall from "./images/Third-FlagSmall.gif";
+import ThirdFlagDistortLarge from "./images/Third-FlagDistortLarge.gif";
 import ThirdTexture from "./images/Texture.gif";
 
 
@@ -41,7 +42,8 @@ render() {
     <div className = "Third-WrapperRed">
     <div className = {this.state.isSceneActive ? 'fadeIn' : 'fadeOut'}>
     <img className = "Third-Texture" src = {ThirdTexture}/>
-    <div className = "Third-FlagDivs"> 
+    <div className = "Third-FlagDivs">
+      <img className = "Third-FlagDistortLarge" src = {ThirdFlagDistortLarge}/>
       <img className = "Third-FlagSmall" src = {ThirdFlagSmall}/>
       <img className = "Third-FlagLarge" src = {ThirdFlagLarge}/>
     </div>
