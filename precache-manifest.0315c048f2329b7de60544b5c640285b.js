271a423572e6d7ab94ff68aaea618f18e3b5977c
nailynevarez <nailynevarez@gmail.com>
2020-05-03 15:59:52 -0700

    Updates

---
+++ precache-manifest.0315c048f2329b7de60544b5c640285b.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c652e5c55c0a4e6d9d5ef6d499f6c373",
+    "url": "/index.html"
+  },
+  {
+    "revision": "77ad5af89d1884a37476",
+    "url": "/static/css/main.4799c6e2.chunk.css"
+  },
+  {
+    "revision": "7614e02eccfb271cb2b7",
+    "url": "/static/js/2.e9793bb8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e9793bb8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77ad5af89d1884a37476",
+    "url": "/static/js/main.1e78ad6c.chunk.js"
+  },
+  {
+    "revision": "c531e2a401b2ede4a814",
+    "url": "/static/js/runtime-main.a039cbf9.js"
+  },
+  {
+    "revision": "7dbfd7b0c9269833e8e0546d31ab5656",
+    "url": "/static/media/First-Background.7dbfd7b0.jpg"
+  },
+  {
+    "revision": "2ec858360f24cef9d140e0d0483ad535",
+    "url": "/static/media/Fourth-BackgroundLarge.2ec85836.jpg"
+  },
+  {
+    "revision": "6fd5e020bea7d37468db6723e8a72724",
+    "url": "/static/media/Fourth-Flag.6fd5e020.png"
+  },
+  {
+    "revision": "5bc190594b94377e1eab38c5989f0d80",
+    "url": "/static/media/General-Cursor.5bc19059.svg"
+  },
+  {
+    "revision": "e4ebbac266297d87d0cd7b9e0cb102fb",
+    "url": "/static/media/Second-Background.e4ebbac2.jpg"
+  },
+  {
+    "revision": "9d931868659e3f2800af4f9b1ea89316",
+    "url": "/static/media/Second-BackgroundLarge.9d931868.jpg"
+  },
+  {
+    "revision": "0676e35e4fe9a9bbe68b82816202c770",
+    "url": "/static/media/Second-BackgroundMedium.0676e35e.jpg"
+  },
+  {
+    "revision": "953510dc82d96826b0af4468b8e2e3a9",
+    "url": "/static/media/Second-BackgroundSmall.953510dc.jpg"
+  },
+  {
+    "revision": "4de957526455e45c3ae67972da7ff3df",
+    "url": "/static/media/Second-Button.4de95752.png"
+  },
+  {
+    "revision": "59c22b8e0127a053c75ff4a181c7081f",
+    "url": "/static/media/Second-Eagle.59c22b8e.gif"
+  },
+  {
+    "revision": "c40cb5f69b61842300ac922540a10a8c",
+    "url": "/static/media/Texture.c40cb5f6.gif"
+  },
+  {
+    "revision": "a227c53d009a89ace802a3078aa91ba7",
+    "url": "/static/media/Third-BackgroundLarge.a227c53d.png"
+  },
+  {
+    "revision": "322dfddde980879bba1d78b6d6e89b89",
+    "url": "/static/media/Third-FlagDistortLarge.322dfddd.gif"
+  },
+  {
+    "revision": "cf0c11652d63c587ee1804e83b0da909",
+    "url": "/static/media/Third-FlagLarge.cf0c1165.gif"
+  },
+  {
+    "revision": "484ff3e25067d51140c07c55372fa2c2",
+    "url": "/static/media/Third-FlagSmall.484ff3e2.gif"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/terminal-project/precache-manifest.93c0283d20257acd70781dc0facd4250.js"
+  "/precache-manifest.0315c048f2329b7de60544b5c640285b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/terminal-project/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
